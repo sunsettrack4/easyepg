0c00500258adbbd026ab3a83c299563e45f05b92
Jan-Luca <sunsettrack4@gmail.com>
2019-05-12 13:43:03 +0200

    Bump version to 0.2.9

--- epg.sh
+++ epg.sh
@@ -22,7 +22,7 @@
 clear
 echo " --------------------------------------------"
 echo " EASYEPG SIMPLE XMLTV GRABBER                "
-echo " Release v0.2.8 BETA - 2019/05/05            "
+echo " Release v0.2.9 BETA - 2019/05/12            "
 echo " powered by                                  "
 echo "                                             "
 echo " ==THE======================================="
