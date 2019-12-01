9b2a4a1aca149bee70198ef5d35a43c1ddec54b8
strowi <roman@hasnoname.de>
2019-12-01 23:01:02 +0100

    + run cron only if $CRON is specified

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,9 +6,10 @@ fi
 
 cd /src/easyepg && "$@"
 
-# create backups
-if [[ ! -f /var/spool/cron/crontabs/root ]]; then
+# run via cron
+if [[ -n "${CRON}" ]]; then
   echo "# setting up cronjob: ${CRON}"
   ( crontab -l; echo "PATH=${PATH}"; echo "${CRON} cd /src/easyepg && /src/easyepg/epg.sh >> /var/log/easyepg.log" ) |crontab -
+else
+  echo "No con schedule specified, exiting..."
 fi
-cron -f
