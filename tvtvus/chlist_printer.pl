7b910548b2220090490f21958abefcb30af4a61f
DeBaschdi <debaschdi@googlemail.com>
2020-01-08 08:02:42 +0100

    Add Provider TVTV-US

---
+++ tvtvus/chlist_printer.pl
@@ -0,0 +1,179 @@
+#!/usr/bin/perl
+
+#      Copyright (C) 2019 Jan-Luca Neumann
+#      https://github.com/sunsettrack4/easyepg
+#
+#      Collaborators:
+#      - DeBaschdi ( https://github.com/DeBaschdi )
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with easyepg. If not, see <http://www.gnu.org/licenses/>.
+
+# ################################
+# TVTV-US CHANNEL LIST CREATOR  #
+# ################################
+
+# CREATE JSON FILE FOR COMPARISM
+
+use strict;
+use warnings;
+ 
+binmode STDOUT, ":utf8";
+use utf8;
+ 
+use JSON;
+
+# READ JSON INPUT FILE: NEW CHLIST
+my $chlist_new;
+{
+    local $/; #Enable 'slurp' mode
+    open my $fh, "<", "/tmp/chlist" or die;
+    $chlist_new = <$fh>;
+    close $fh;
+}
+
+# READ JSON INPUT FILE: OLD CHLIST
+my $chlist_old;
+{
+    local $/; #Enable 'slurp' mode
+    open my $fh, "<", "chlist_old" or die;
+    $chlist_old = <$fh>;
+    close $fh;
+}
+
+# READ JSON INPUT FILE: CHANNEL CONFIG
+my $chlist_config;
+{
+    local $/; #Enable 'slurp' mode
+    open my $fh, "<", "channels.json" or die;
+    $chlist_config = <$fh>;
+    close $fh;
+}
+
+# CONVERT JSON TO PERL STRUCTURES
+my $newdata      = decode_json($chlist_new);
+my $olddata      = decode_json($chlist_old);
+my $configdata   = decode_json($chlist_config);
+
+
+# ##################
+# NEW CHANNEL LIST #
+# ##################
+
+# TOOL: NAME ==> ID
+
+print "{ \"newname2id\": {\n";
+
+my @newchannels_name2id = @{ $newdata->{'stations'} };
+foreach my $newchannels ( @newchannels_name2id ) {
+
+	#
+	# DEFINE JSON VALUES
+	#
+        
+	# DEFINE NEW CHANNEL NAME
+	my $newcname   = $newchannels->{'name'};
+	$newcname =~ s/\&/\&amp;/g; # REQUIRED TO READ XML FILE CORRECTLY
+		
+	# DEFINE NEW CHANNEL ID
+	my $newcid     = $newchannels->{'channelNumber'};
+		
+	# PRINT NEW CHANNEL NAMES
+	print "\"$newcname\": \"$newcid\",\n";
+}
+
+# TOOL: ID ==> NAME
+
+print "\"DUMMY\": \"DUMMY\" },\n\"newid2name\": {\n";
+
+my @newchannels_id2name = @{ $newdata->{'stations'} };
+foreach my $newchannels ( @newchannels_id2name ) {
+		
+	#
+	# DEFINE JSON VALUES
+	#
+        
+	# DEFINE NEW CHANNEL NAME
+	my $newcname   = $newchannels->{'name'};
+	$newcname =~ s/\&/\&amp;/g; # REQUIRED TO READ XML FILE CORRECTLY
+		
+	# DEFINE NEW CHANNEL ID
+	my $newcid     = $newchannels->{'channelNumber'};
+		
+	# PRINT NEW CHANNEL NAMES newcid
+	print "\"$newcid\": \"$newcname\",\n";
+}
+
+				
+# ##################
+# OLD CHANNEL LIST #
+# ##################
+
+# TOOL: NAME ==> ID
+
+print "\"DUMMY\": \"DUMMY\" },\n\"oldname2id\": {\n";
+						
+my @oldchannels_name2id = @{ $olddata->{'stations'} };
+foreach my $oldchannels ( @oldchannels_name2id ) {
+						
+	#
+	# DEFINE JSON VALUES
+	#
+								
+	# DEFINE OLD CHANNEL NAME
+	my $oldcname   = $oldchannels->{'name'};
+	$oldcname =~ s/\&/\&amp;/g; # REQUIRED TO READ XML FILE CORRECTLY
+		
+	# DEFINE OLD CHANNEL ID
+	my $oldcid     = $oldchannels->{'channelNumber'};
+							
+	# PRINT OLD CHANNEL NAMES
+	print "\"$oldcname\": \"$oldcid\",\n";
+}
+
+# TOOL: ID ==> NAME
+
+print "\"DUMMY\": \"DUMMY\" },\n\"oldid2name\": {\n";
+						
+my @oldchannels_id2name = @{ $olddata->{'stations'} };
+foreach my $oldchannels ( @oldchannels_id2name ) {
+						
+	#
+	# DEFINE JSON VALUES
+	#
+								
+	# DEFINE OLD CHANNEL NAME
+	my $oldcname   = $oldchannels->{'name'};
+	$oldcname =~ s/\&/\&amp;/g; # REQUIRED TO READ XML FILE CORRECTLY
+		
+	# DEFINE OLD CHANNEL ID
+	my $oldcid     = $oldchannels->{'channelNumber'};
+							
+	# PRINT OLD CHANNEL NAMES
+	print "\"$oldcid\": \"$oldcname\",\n";
+}	
+
+							
+# #######################
+# CHANNEL CONFIGURATION #
+# #######################
+
+print "\"DUMMY\": \"DUMMY\" },\n\"config\": [\n";	
+												
+my @configdata = @{ $configdata->{'channels'} };
+		
+foreach my $configname ( @configdata ) {
+	print "\"$configname\",\n";
+}
+
+print "\"DUMMY\"]\n}";
---
+++ tvtvus/tvtvus.sh
@@ -0,0 +1,456 @@
+#!/bin/bash
+
+#      Copyright (C) 2019 Jan-Luca Neumann
+#      https://github.com/sunsettrack4/easyepg
+#
+#      Collaborators:
+#      - DeBaschdi ( https://github.com/DeBaschdi )
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with easyepg. If not, see <http://www.gnu.org/licenses/>.
+
+
+# ################
+# INITIALIZATION #
+# ################
+
+#
+# SETUP ENVIRONMENT
+#
+
+mkdir cache 2> /dev/null	# cache
+mkdir day 2> /dev/null		# download scripts
+mkdir mani 2> /dev/null		# manifest files
+
+if grep -q "USA" init.json 2> /dev/null
+then
+	printf "+++ COUNTRY: USA +++\n\n"
+fi
+
+if grep -q '"day": "0"' settings.json
+then
+	printf "EPG Grabber disabled!\n\n"
+	exit 0
+fi
+
+if ! curl --write-out %{http_code} --silent --output /dev/null https://tvtv.us/ | grep -q "200"
+then
+	printf "Service provider unavailable!\n\n"
+	exit 0
+fi
+
+
+# ##################
+# DOWNLOAD PROCESS #
+# ##################
+
+echo "- DOWNLOAD PROCESS -" && echo ""
+
+#
+# DELETE OLD FILES
+#
+
+printf "\rDeleting old files...                       "
+
+rm -rf cache/ 2> /dev/null
+rm -rf day/ 2> /dev/null
+rm -rf mani/ 2> /dev/null
+rm /tmp/epg_workfile 2> /dev/null
+rm /tmp/workfile 2> /dev/null
+rm /tmp/workfile2 2> /dev/null
+
+mkdir cache 2> /dev/null
+mkdir day 2> /dev/null
+mkdir mani 2> /dev/null
+
+
+#
+# LOADING MANIFEST FILES
+#
+
+printf "\rFetching channel list...               "
+curl --compressed -s https://tvtv.us/tvm/t/tv/v4/lineups/2381D/ > /tmp/workfile
+jq '.' /tmp/workfile > /tmp/chlist
+
+
+printf "\rChecking manifest files... "
+perl chlist_printer.pl > /tmp/compare.json
+perl url_printer.pl 2>errors.txt | sed '/DUMMY/d' > mani/common
+
+printf "\n$(echo $(wc -l < mani/common)) manifest file(s) to be downloaded!\n\n"
+
+if [ $(wc -l < mani/common) -ge 7 ]
+then
+	number=$(echo $(( $(wc -l < mani/common) / 7)))
+	
+	split -l $number --numeric-suffixes mani/common mani/day
+
+else	
+	cp mani/common mani/day00
+fi
+
+#
+# CREATE STATUS INFO FOR MANIFEST FILE DOWNLOAD
+#
+cp mani/common common
+
+function status_manifest_download { 
+    #setup_scroll_area
+	sleep 2 2> /dev/null ;
+    thread=$(ps ax)
+	if [[ $thread =~ ^.*curl.*$ ]] ;
+        then 
+			z0="[                    ]"
+			z5="[#                   ]"
+			z10="[##                  ]"
+			z15="[###                 ]"
+			z20="[####                ]"
+			z25="[#####               ]"
+			z30="[######              ]"
+			z35="[#######             ]"
+			z40="[########            ]"
+			z45="[#########           ]"
+			z50="[##########          ]"
+			z55="[###########         ]"
+			z60="[############        ]"
+			z65="[#############       ]"
+			z70="[##############      ]"
+			z75="[###############     ]"
+			z80="[################    ]"
+			z85="[#################   ]"
+			z90="[##################  ]"
+			z95="[################### ]"
+			z100="[####################]"
+
+			df=$(find mani/ -type f | wc -l) ;
+			ftd=$(wc -l < mani/common) ;
+			status=$(expr $df \* 100 / $ftd - 2) ;
+			if [[ $status -gt 100 || $status -eq 100 ]]; then status="100"; fi
+			if [[ $status -gt 0 && $status -lt 5 || $status -eq 0 ]]; then bar="$z0"; elif [[ $status -gt 5 && $status -lt 10 ]]; then bar="$z5"; elif [[ $status -gt 10 && $status -lt 15 ]] ; then bar="$z10"; elif [[ $status -gt 15 && $status -lt 20 ]] ; then bar="$z15"; elif [[ $status -gt 20 && $status -lt 25 ]] ; then bar="$z20"; elif [[ $status -gt 25 && $status -lt 30 ]] ; then bar="$z25"; elif [[ $status -gt 30 && $status -lt 35 ]] ; then bar="$z30"; elif [[ $status -gt 35 && $status -lt 40 ]] ; then bar="$z35"; elif [[ $status -gt 40 && $status -lt 45 ]] ; then bar="$z40"; elif [[ $status -gt 40 && $status -lt 50 ]] ; then bar="$z45"; elif [[ $status -gt 50 && $status -lt 55 ]] ; then bar="$z50"; elif [[ $status -gt 55 && $status -lt 60 ]] ; then bar="$z55"; elif [[ $status -gt 60 && $status -lt 65 ]] ; then bar="$z60"; elif [[ $status -gt 60 && $status -lt 70 ]] ; then bar="$z65"; elif [[ $status -gt 70 && $status -lt 75 ]] ; then bar="$z70"; elif [[ $status -gt 70 && $status -lt 80 ]] ; then bar="$z75"; elif [[ $status -gt 80 && $status -lt 85 ]] ; then bar="$z80"; elif [[ $status -gt 85 && $status -lt 90 ]] ; then bar="$z85"; elif [[ $status -gt 90 && $status -lt 95 ]] ; then bar="$z90"; elif [[ $status -gt 95 && $status -lt 100 ]] ; then bar="$z95"; elif [ $status -eq 100 ] ; then bar="$z100";fi
+			printf "\rProgress $bar  $status%% ";                                  
+			status_manifest_download ;
+        fi
+        }
+
+#
+# CREATE MANIFEST DOWNLOAD SCRIPTS
+#
+
+for time in {0..8..1}
+do	
+	sed -i '1i#\!\/bin\/bash\n' mani/day0${time} 2> /dev/null
+done
+
+
+#
+# COPY/PASTE EPG DETAILS
+#
+
+printf "\rLoading manifest files..."
+echo ""
+printf "\rProgress [                    ]    0%% "
+
+status_manifest_download &
+
+for a in {0..8..1}
+do
+	bash mani/day0${a} 2> /dev/null &
+done
+wait
+
+rm mani/day0* 2> /dev/null && rm mani/common 2> /dev/null
+
+printf "\rProgress [####################]  100%% "
+echo "DONE!" && printf "\n"
+
+
+#
+# CREATE EPG BROADCAST LIST
+#
+
+printf "\rCreating EPG manifest file... "
+
+rm /tmp/manifile.json 2> /dev/null
+cat mani/* > /tmp/manifile.json
+cp /tmp/manifile.json manifile.json
+jq  -s '.' /tmp/manifile.json > /tmp/epg_workfile 2>>errors.txt
+sed -i '1s/\[/{ "attributes":[/g;$s/\]/&}/g' /tmp/epg_workfile
+
+cp /tmp/epg_workfile epg_workfile.json
+rm mani/*
+echo "DONE!" && printf "\n"
+perl compare_crid.pl > day/daydlnew
+
+#
+# SHOW ERROR MESSAGE + ABORT PROCESS IF CHANNEL IDs WERE CHANGED
+#
+
+sort -u errors.txt > /tmp/errors_sorted.txt && mv /tmp/errors_sorted.txt errors.txt
+
+if [ -s errors.txt ] 
+then
+	echo "================= CHANNEL LIST: LOG ==================="
+	echo ""
+	
+	input="errors.txt"
+	while IFS= read -r var
+	do
+		echo "$var"
+	done < "$input"
+	
+	echo ""
+	echo "======================================================="
+	echo ""
+	
+	cp /tmp/chlist chlist_old
+else
+	rm errors.txt 2> /dev/null
+fi
+
+#
+# DOWNLOAD EPG DETAILS
+#
+
+printf "\rPreparing multithreaded download...                   "
+
+sed "s/.*/curl -s --compressed --connect-timeout 2 --max-time 10 --retry 8 --retry-delay 0 --retry-max-time 5  'https:\/\/tvtv.us\/tvm\/t\/tv\/v4\/episodes\/&' | grep 'seriesID' > cache\/&/g" day/daydlnew > day/common
+
+sed -i '/^$/d' day/common
+sort -u day/common | uniq > /tmp/common
+cp /tmp/common day/common
+printf "\n$(echo $(wc -l < day/common)) broadasts files to be downloaded!\n\n"
+if [ $(wc -l < day/common) -ge 32 ]
+then
+	number=$(echo $(( $(wc -l < day/common) / 32)))
+
+	split -l $number --numeric-suffixes day/common day/day
+
+else
+	cp day/common day/day00
+fi
+
+
+#
+# CREATE STATUS BAR FOR DOWNLOAD SCRIPT
+#
+
+function status_detail_download { 
+    #setup_scroll_area
+	sleep 2 2> /dev/null ;
+    thread=$(ps ax)
+	if [[ $thread =~ ^.*curl.*$ ]] ;
+        then 
+			z0="[                    ]"
+			z5="[#                   ]"
+			z10="[##                  ]"
+			z15="[###                 ]"
+			z20="[####                ]"
+			z25="[#####               ]"
+			z30="[######              ]"
+			z35="[#######             ]"
+			z40="[########            ]"
+			z45="[#########           ]"
+			z50="[##########          ]"
+			z55="[###########         ]"
+			z60="[############        ]"
+			z65="[#############       ]"
+			z70="[##############      ]"
+			z75="[###############     ]"
+			z80="[################    ]"
+			z85="[#################   ]"
+			z90="[##################  ]"
+			z95="[################### ]"
+			z100="[####################]"
+
+			df=$(find cache -type f | wc -l) ;
+			ftd=$(wc -l < day/common) ;
+			status=$(expr $df \* 100 / $ftd) ;
+			if [[ $status -gt 100 || $status -eq 100 ]]; then status="100"; fi
+			if [[ $status -gt 0 && $status -lt 5 || $status -eq 0 ]]; then bar="$z0"; elif [[ $status -gt 5 && $status -lt 10 ]]; then bar="$z5"; elif [[ $status -gt 10 && $status -lt 15 ]] ; then bar="$z10"; elif [[ $status -gt 15 && $status -lt 20 ]] ; then bar="$z15"; elif [[ $status -gt 20 && $status -lt 25 ]] ; then bar="$z20"; elif [[ $status -gt 25 && $status -lt 30 ]] ; then bar="$z25"; elif [[ $status -gt 30 && $status -lt 35 ]] ; then bar="$z30"; elif [[ $status -gt 35 && $status -lt 40 ]] ; then bar="$z35"; elif [[ $status -gt 40 && $status -lt 45 ]] ; then bar="$z40"; elif [[ $status -gt 40 && $status -lt 50 ]] ; then bar="$z45"; elif [[ $status -gt 50 && $status -lt 55 ]] ; then bar="$z50"; elif [[ $status -gt 55 && $status -lt 60 ]] ; then bar="$z55"; elif [[ $status -gt 60 && $status -lt 65 ]] ; then bar="$z60"; elif [[ $status -gt 60 && $status -lt 70 ]] ; then bar="$z65"; elif [[ $status -gt 70 && $status -lt 75 ]] ; then bar="$z70"; elif [[ $status -gt 70 && $status -lt 80 ]] ; then bar="$z75"; elif [[ $status -gt 80 && $status -lt 85 ]] ; then bar="$z80"; elif [[ $status -gt 85 && $status -lt 90 ]] ; then bar="$z85"; elif [[ $status -gt 90 && $status -lt 95 ]] ; then bar="$z90"; elif [[ $status -gt 95 && $status -lt 100 ]] ; then bar="$z95"; elif [ $status -eq 100 ] ; then bar="$z100";fi
+			printf "\rProgress $bar  $status%% ";                                  
+			status_detail_download ;
+        fi
+        }
+
+
+#
+# CREATE DOWNLOAD SCRIPTS
+#
+
+for time in {0..33..1}
+do	
+	sed -i '1s/.*/#\!\/bin\/bash\n&/g' day/day0${time} 2> /dev/null & sed -i '1s/.*/#\!\/bin\/bash\n&/g' day/day0${time} 2> /dev/null 
+done
+
+
+#
+# DOWNLOAD EPG DETAILS
+#
+
+printf "\rDownloading EPG details...                 "
+echo ""
+printf "\rProgress [                    ]    0%% "
+
+status_detail_download &
+
+for a in {0..33..1}
+do
+	bash day/day0${a} 2> /dev/null & bash day/day${a} 2> /dev/null & 
+done
+wait
+
+rm day/*
+
+printf "\rProgress [####################]  100%% "
+echo  "DONE!" && printf "\n"
+
+#
+# EXPORT 0BYTE EPGDETAILS AND TRY TO REDOWNLOAD
+#
+
+find cache -size 0 | sed 's/cache\///g' >missingbroadcasts
+touch broadcast_warnings.txt
+
+if [ -s missingbroadcasts ]
+then
+	echo  "Missing Broadcastfiles Detected" && printf "\n"
+	printf "\rWaiting for 10 Seconds"
+	sleep 10
+	printf "\rPreparing Broadcastdatabase (TVTV seems to hate Screen-Scrapper)...                 "
+	echo ""
+	find cache -size 0 | sed 's/cache\///g' >day/daydlnew
+	sed "s/.*/curl -s --compressed --connect-timeout 2 --max-time 10 --retry 8 --retry-delay 0 --retry-max-time 5  'https:\/\/tvtv.us\/tvm\/t\/tv\/v4\/episodes\/&' | grep 'seriesID' > cache\/&/g" day/daydlnew > day/common
+	sed -i '1s/.*/#\!\/bin\/bash\n&/g' day/common 2> /dev/null
+	sed -i '/^$/d' day/common
+	printf "\n$(echo $(wc -l < day/common)) missing Broadastsfiles to be downloaded!\n\n"
+	bash day/common 2> /dev/null & wait
+	find cache -size 0 | sed 's/cache\///g' >missingbroadcasts
+	if [ -s missingbroadcasts ]
+	then
+		sed 's/.*/\[ EPG WARNING ] FAILED TO DOWNLOAD BROADCASTFILES &/g' missingbroadcasts >broadcast_warnings.txt
+	fi		
+fi
+
+rm missingbroadcasts
+rm day/* 2> /dev/null
+
+echo  "DONE!" && printf "\n"
+
+# ###################
+# CREATE XMLTV FILE #
+# ###################
+
+# WORK IN PROGRESS
+
+echo "- FILE CREATION PROCESS -" && echo ""
+
+rm workfile chlist 2> /dev/null
+
+
+# DOWNLOAD CHANNEL LIST + RYTEC/EIT CONFIG FILES (JSON)
+printf "\rRetrieving channel list and config files...          "
+curl --compressed -s https://tvtv.us/tvm/t/tv/v4/lineups/2381D/ > /tmp/chlist
+jq '.' /tmp/chlist > chlist
+
+cp chlist /tmp/chlist
+curl -s https://raw.githubusercontent.com/sunsettrack4/config_files/master/tvtvus_channels.json > tvtvus_channels.json
+curl -s https://raw.githubusercontent.com/sunsettrack4/config_files/master/tvtvus_genres.json > tvtvus_genres.json
+
+# CONVERT JSON INTO XML: CHANNELS
+printf "\rConverting CHANNEL JSON file into XML format...      "
+perl ch_json2xml.pl 2>warnings.txt > tvtv-us_channels
+sort -u tvtv-us_channels > /tmp/tvtv-us_channels && mv /tmp/tvtv-us_channels tvtv-us_channels
+sed -i 's/></>\n</g;s/<display-name/  &/g' tvtv-us_channels
+
+# CREATE CHANNEL ID LIST AS JSON FILE
+printf "\rRetrieving Channel IDs...                    "
+perl cid_json.pl > tvtvus_cid.json && rm chlist
+
+# CONVERT JSON INTO XML: EPG
+printf "\rConverting EPG JSON file into XML format...          "
+perl epg_json2xml.pl > tvtv-us_epg 2>epg_warnings.txt && rm /tmp/epg_workfile 2> /dev/null
+
+
+# COMBINE: CHANNELS + EPG
+printf "\rCreating EPG XMLTV file...                   "
+cat tvtv-us_epg >> tvtv-us_channels && mv tvtv-us_channels tvtv-us && rm tvtv-us_epg
+sed -i '1i<?xml version="1.0" encoding="UTF-8" ?>\n<\!-- EPG XMLTV FILE CREATED BY THE EASYEPG PROJECT - (c) 2019 Jan-Luca Neumann -->\n<tv>' tvtv-us
+sed -i "s/<tv>/<\!-- created on $(date) -->\n&\n\n<!-- CHANNEL LIST -->\n/g" tvtv-us
+sed -i '$s/.*/&\n\n<\/tv>/g' tvtv-us
+mv tvtv-us tvtv-us.xml
+
+# VALIDATING XML FILE
+printf "\rValidating EPG XMLTV file..."
+xmllint --noout tvtv-us.xml > errorlog 2>&1
+
+if grep -q "parser error" errorlog
+then
+	printf " DONE!\n\n"
+	mv tvtv-us.xml tvtv-us_ERROR.xml
+	echo "[ EPG ERROR ] XMLTV FILE VALIDATION FAILED DUE TO THE FOLLOWING ERRORS:" >> warnings.txt
+	cat errorlog >> warnings.txt
+else
+	printf " DONE!\n\n"
+	rm tvtv-us_ERROR.xml 2> /dev/null
+	rm errorlog 2> /dev/null
+	
+	if ! grep -q "<programme start=" tvtv-us.xml
+	then
+		echo "[ EPG ERROR ] XMLTV FILE DOES NOT CONTAIN ANY PROGRAMME DATA!" >> errorlog
+	fi
+	
+	if ! grep "<channel id=" tvtv-us.xml > /tmp/id_check
+	then
+		echo "[ EPG ERROR ] XMLTV FILE DOES NOT CONTAIN ANY CHANNEL DATA!" >> errorlog
+	fi
+	
+	uniq -d /tmp/id_check > /tmp/id_checked
+	if [ -s /tmp/id_checked ]
+	then
+		echo "[ EPG ERROR ] XMLTV FILE CONTAINS DUPLICATED CHANNEL IDs!" >> errorlog
+		sed -i 's/.*/[ DUPLICATE ] &/g' /tmp/id_checked && cat /tmp/id_checked >> errorlog
+		rm /tmp/id_check /tmp/id_checked 2> /dev/null
+	else
+		rm /tmp/id_check /tmp/id_checked 2> /dev/null
+	fi
+	
+	if [ -e errorlog ]
+	then
+		mv tvtv-us.xml tvtv-us_ERROR.xml
+		cat errorlog >> warnings.txt
+	else
+		rm errorlog 2> /dev/null
+	fi
+fi
+
+
+# SHOW WARNINGS
+cat epg_warnings.txt >> warnings.txt && rm epg_warnings.txt
+sort -u warnings.txt > sorted_warnings.txt && mv sorted_warnings.txt warnings.txt
+sed -i '/^$/d' warnings.txt
+
+if [ -s warnings.txt ]
+then
+	echo "========== EPG CREATION: WARNING/ERROR LOG ============"
+	echo ""
+	
+	input="warnings.txt"
+	while IFS= read -r var
+	do
+		echo "$var"
+	done < "$input"
+	
+	echo ""
+	echo "======================================================="
+	echo ""
+fi
