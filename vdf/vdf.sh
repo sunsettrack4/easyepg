7c02c2082954efa186151f3ad003e5c84ad06956
Jan-Luca Neumann <sunsettrack4@gmail.com>
2020-01-28 20:48:08 +0100

    Vodafone: Speed up sorting process

--- vdf/vdf.sh
+++ vdf/vdf.sh
@@ -386,10 +386,8 @@ find cache/ -type f -print0 | xargs -0 cat >/tmp/workfile 2> /dev/null
 
 # SORT BY CID AND START TIME
 printf "\rSorting data by channel ID and start time...         "
-sed -i 's/\(.*\)\("startDateTimeMillis":[^,]*,\)\(.*\)/\2\1\2\3/g' /tmp/workfile 2> /dev/null
-sed -i 's/\(.*\)\("channelId":[^,]*,\)\(.*\)/\2\1\2\3/g' /tmp/workfile 2> /dev/null
-sort -u /tmp/workfile > /tmp/workfile2 2> /dev/null
-sed 's/\(.*\)\({"_id".*\)/\2/g' /tmp/workfile2 > /tmp/workfile 2> /dev/null
+sed 's/{\("_id":[^,]*,\)\(.*\)\("startDateTimeMillis":[^,]*,\)\(.*\)\("channelId":[^,]*,\)\(.*\)/{\5\3\1\2\4\6/g' /tmp/workfile > /tmp/workfile2 2> /dev/null
+sort -u /tmp/workfile2 > /tmp/workfile 2> /dev/null
 
 
 # VALIDATE JSON FILE
